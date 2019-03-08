4b8afd0fdccbce1391da18e6dd811a41989872eb
Jason-Lawrence <lawjason28@yahoo.com>
2019-03-08 01:42:58 -0600

    minor bug with object creation

--- CoulombsLaw/bin/CoulombsLaw.py
+++ CoulombsLaw/bin/CoulombsLaw.py
@@ -1,8 +1,18 @@
 import math
-import Utiliy
+import Utility
 import Atom
+import os
 
 def main():
-    Atoms, Ligands = Utility.init() # This will return two lists, one for all of the Ligands and another for the General Atoms
+    dir = "C:\\Users\\Jason\\Desktop\\Projects\\Research\\CoulombsLaw\\Files\\Input" # This is the directory with all of the input files to run
+    for file in os.listdir(dir):
+        Atoms, Ligands = Utility.init(dir, file) # This will return two lists, one for all of the Ligands and another for the General Atoms
+        print("The length of the Atoms subset is: " + str(len(Atoms)))
+        print("The length of the Ligands subset is: " + str(len(Ligands)))
+        #results = init(Atoms, Ligands)
+        #Utility.outputGeneration(results)
+
+def init(Atoms, Ligands):
+    print("hello")
 
 main()
--- CoulombsLaw/bin/Utility.py
+++ CoulombsLaw/bin/Utility.py
@@ -1,10 +1,35 @@
-def init():
-    dir = "C:\\Users\\Jason\\Desktop\\Projects\\Research\\CoulombsLaw\\Files" ## replace this with where ever the file is located
-    file = open(dir + "\\complex.pqr") #replace this with the file name
-    return Constructor(file)
+import Atom
+
+def init(dir, fileName):
+    filePath = dir + "\\" + fileName
+    with open(filePath, 'r') as file:
+        Atoms, Ligands = Constructor(file)
+    file.close()
+    return Atoms, Ligands
 
 def Constructor(file):
-     Atoms   = []
-     Ligands = []
+    Atoms   = []
+    Ligands = []
+    for line in file.readlines():
+        print(line)
+        atomInfo = parser(line)
+        if len(atomInfo) == 10: #True if there is no Chain ID. Chain ID will be set to an empty string ""
+            atomInfo.insert(4, "")
+        atom = buildAtom(atomInfo)
 
-     for line
+        if line[2] == "LIG":
+            Ligands.append(atom)
+        else:
+            Atoms.append(atom)
+
+    return Atoms, Ligands
+
+def parser(line):
+    parsed = " ".join(line.split())
+    print(parsed)
+    parsedLine = parsed.split(" ")
+    print(parsedLine)
+    return parsedLine
+
+def buildAtom(atomInfo):
+    return Atom.Atom(atomInfo[0], int(atomInfo[1]), atomInfo[2], atomInfo[3], atomInfo[4], int(atomInfo[5]), float(atomInfo[6]), float(atomInfo[7]), float(atomInfo[8]), float(atomInfo[9]), float(atomInfo[10])) 
